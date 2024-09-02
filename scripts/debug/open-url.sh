4f358fafe46be11a1a1e2f740f036fa3c8fd07eb
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-09-02 10:05:22 -0700

    stash changes

---
+++ scripts/debug/open-url.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o xtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# example usage:
+# ./scripts/debug/open-url.sh
+# fetch file "out" from the remote server, and open all the URLs in the file
+
+REMOTE_SERVER="xc"
+REMOTE_FILE="/home/xiaochen/code/cockroach/out"
+
+scp "${REMOTE_SERVER}:${REMOTE_FILE}" /tmp/out
+
+while read -r line; do
+    # open it if it starts with "http"
+    if [[ $line == http* ]]; then
+        open "$line"
+    fi
+done < /tmp/out
