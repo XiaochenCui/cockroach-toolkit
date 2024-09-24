13a6e9994285c0c8b21de405b5c8e586cb669171
XiaochenCui <jcnlcxc.new@gmail.com>
2024-09-24 13:47:17 -0700

    update

--- scripts/debug/xiaochen-patch.py
+++ scripts/debug/xiaochen-patch.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# Usage:
+#   python3 xiaochen-patch.py <on|off>
+#
+# This script is used to patch the scaffold code to the CockroachDB source code.
+#
+# Current patches:
+#   - update package "pkg/roachprod/logger" to make the debugging easier
+#   - disable enterprise license check
+
 import os
 import shutil
 import sys
@@ -43,11 +52,37 @@ def patch_file(file_path, mode: str):
             print(f"File removed from {target_location}")
 
 
+def toggle_enterprise_license_check(mode: str):
+    target_path = os.path.join(COCKROACH_SRC_PATH, "pkg/ccl/utilccl/license_check.go")
+
+    patch = "return nil"
+    origin = "return checkEnterpriseEnabledAt(st, timeutil.Now(), feature, true /* withDetails */)"
+
+    match mode:
+        case "on":
+            replace_string(target_path, origin, patch)
+            print("Enterprise license check is disabled")
+        case "off":
+            replace_string(target_path, patch, origin)
+            print("Enterprise license check is enabled")
+
+
+def replace_string(file_path, old_string, new_string):
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    content = content.replace(old_string, new_string)
+
+    with open(file_path, "w") as file:
+        file.write(content)
+
+
 if __name__ == "__main__":
     # the first argument is the mode
     if len(sys.argv) != 2:
-        print("Usage: turn-debug.py <on|off>")
+        print("Usage: xiaochen-patch.py <on|off>")
         sys.exit(1)
 
     mode = sys.argv[1]
     patch_all_files(mode)
+    toggle_enterprise_license_check(mode)
